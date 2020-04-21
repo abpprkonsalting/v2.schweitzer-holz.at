aff6a8ea672a14df9abcc4cb5147df778025a207
Armando Baños Pascual <abpprkonsalting@gmail.com>
2020-04-21 15:23:04 -0400

    dividing themes

---
+++ app/design/frontend/Webdirect/Schweitzer/web/js/schweitzer-custom.js
@@ -0,0 +1,9 @@
+
+require(['jquery', 'loader'], function($){ 
+    $(document).ready(function ($) {
+
+        $('body').trigger('processStart');
+        //$("body").loader("show");
+        console.log('bla');
+    });
+});
