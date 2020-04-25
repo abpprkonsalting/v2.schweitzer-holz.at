121f9ed3bd42c49612885fc29cddb33e86f71985
Armando Baños Pascual <abpprkonsalting@gmail.com>
2020-04-25 03:59:09 -0400

    css in mobile menu

--- app/code/ABPPRK/LoadingSpinner/view/frontend/web/js/abpspinner.js
+++ app/code/ABPPRK/LoadingSpinner/view/frontend/web/js/abpspinner.js
@@ -6,8 +6,8 @@ define([
         return function(config, element) {
             $(document).ready(function ($) {
                 window.abpSpinnerTimer = window.setInterval(function(){
-                    var condition = $(".navigation ul").css("background-color");
-                    if (condition == "rgb(0, 114, 85)") {
+                    var condition = $(".page-footer").css("background-color");
+                    if (condition == "rgb(170, 160, 142)") {
                         $('.abp-spinner-wrapper').css('opacity',0);
                         window.setTimeout(function(){$('.abp-spinner-wrapper').css('display','none');$('.abp-spinner-wrapper').css('z-index','0');},1000);
                         clearInterval(window.abpSpinnerTimer);
