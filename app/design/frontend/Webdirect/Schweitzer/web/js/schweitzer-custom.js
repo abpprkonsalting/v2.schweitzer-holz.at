a4373ba0b94d501132f1ba8d9790e9d47730eb11
Armando Baños Pascual <abpprkonsalting@gmail.com>
2020-05-01 00:44:12 -0400

    second addToCart Button on product view

--- app/design/frontend/Webdirect/Schweitzer/web/js/schweitzer-custom.js
+++ app/design/frontend/Webdirect/Schweitzer/web/js/schweitzer-custom.js
@@ -2,6 +2,17 @@
 require(['jquery'], function($){ 
     $(document).ready(function ($) {
 
+        var secondAddToCartButton = jQuery(".second-add-to-cart-button button");
+        secondAddToCartButton.attr("id","product-addtocart-button-second");
+        if (secondAddToCartButton.length > 0) {
+            secondAddToCartButton.click(function(){
+                var realAddToCartButton = jQuery("#product-addtocart-button");
+                if (realAddToCartButton.length > 0) {
+                    realAddToCartButton.click();
+                    $("html, body").animate({ scrollTop: 0 }, "slow");
+                }
+            });
+        }
 
     });
 });
